a2401d053952dcef6ab53f6d4c05b5d09d6d1e88
Steve Mattsen <smattsen@affirma.com>
2021-12-03 11:35:32 -0600

    Completed the exercise.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -24,6 +24,26 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+        };
+      }
+    },
+  }
 };
 
 module.exports = resolvers;
